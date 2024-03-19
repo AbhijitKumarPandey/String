9bbf22a828d4e06921f02322889577b0cb922704
AbhijitKumarPandey <156564538+AbhijitKumarPandey@users.noreply.github.com>
2024-03-19 21:05:14 +0530

    Add files via upload

---
+++ concatenation.java
@@ -0,0 +1,10 @@
+
+public class concatenation {
+	public static void main(String[] args) {
+		String firstName = "Abhijit";
+		String lastName = "Pandey";
+		String fullName = firstName + " " + lastName;
+		System.out.println(fullName);
+	}
+
+}
